9e51204ab17a537e5fc03795453e9d2f9b8083eb
servemyego <lifeofego@gmail.com>
2025-08-04 01:19:31 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,12 +4,10 @@ let leftButton, rightButton;
 let leftHandImg, rightHandImg;
 
 function preload() {
-  // โหลดภาพตัวละคร
-  images[0] = loadImage('woman_8bit.png');      // เริ่มต้น
-  images[1] = loadImage('alt_image_1.png');     // ซ้าย
-  images[2] = loadImage('alt_image_2.png');     // ขวา
+  images[0] = loadImage('woman_8bit.png');
+  images[1] = loadImage('alt_image_1.png');
+  images[2] = loadImage('alt_image_2.png');
 
-  // โหลดภาพปุ่มมือ
   leftHandImg = loadImage('left-hand.png');
   rightHandImg = loadImage('right-hand.png');
 }
@@ -22,22 +20,10 @@ function setup() {
   rightButton = { x: width - 80, y: height - 80, size: 80 };
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-
 function draw() {
-  // พื้นหลังตามสถานะ
-  if (currentImage === 0) {
-    background('#1E90FF'); // ฟ้า
-  } else {
-    background('#FF4444'); // แดง
-  }
+  background(currentImage === 0 ? '#1E90FF' : '#FF4444');
+  image(images[currentImage], width/2, height/2);
 
-  // แสดงภาพตรงกลาง
-  image(images[currentImage], width / 2, height / 2);
-
-  // แสดงปุ่มซ้ายขวา
   image(leftHandImg, leftButton.x, leftButton.y, leftButton.size, leftButton.size);
   image(rightHandImg, rightButton.x, rightButton.y, rightButton.size, rightButton.size);
 }
@@ -53,15 +39,9 @@ function mousePressed() {
 }
 
 function mouseReleased() {
-  currentImage = 0; // กลับเป็นรูปเริ่มต้น
+  currentImage = 0;
 }
 
-function touchStarted() {
-  if (dist(mouseX, mouseY, leftButton.x, leftButton.y) < leftButton.size / 2) {
-    currentImage = 1;
-  }
-
-  if (dist(mouseX, mouseY, rightButton.x, rightButton.y) < rightButton.size / 2) {
-    currentImage = 2;
-  }
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
